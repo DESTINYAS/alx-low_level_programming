d5831f85089ee565022cd943ab72b5a9eb7eb667
DESTINYAS <destiny.ochonogor@gmail.com>
2022-06-04 21:41:18 -0700

    task 4 correct

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,17 @@
+/*
+ * Filename:4-puts.c
+ * Author:DESTINYAS
+ */
+
 #include <stdio.h>
 
+/**
+ * main - prints "Programming is like building a multilingual puzzle
+ * Return: 0.
+ */
 int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);	
-
+	return (0);	
 }
