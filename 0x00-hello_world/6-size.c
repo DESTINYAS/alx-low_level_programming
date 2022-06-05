23b395e1759cb5c0d7ea8b3d252d3b76d772b4cb
DESTINYAS <destiny.ochonogor@gmail.com>
2022-06-05 13:34:23 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@
  */
 int main(void)
 {
-	fprintf("size of a char: %zu byte(s)\n", sizeof(char));
-	fprintf("size of an int: %zu byte(s)\n", sizeof(int));
-	fprintf("size of a long int: %zu byte(s)\n", sizeof(long int));
-	fprintf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	fprintf("size of a float: %zu byte(s)\n", sizeof(float));
+	printf("size of a char: %zu byte(s)\n", sizeof(char));
+	printf("size of an int: %zu byte(s)\n", sizeof(int));
+	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
