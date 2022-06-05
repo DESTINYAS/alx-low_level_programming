258dbd9636e4c4bb8dfce70a64b8ad80659e8c64
DESTINYAS <destiny.ochonogor@gmail.com>
2022-06-05 14:19:33 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+/*
+ * file: 101-quote.c
+ * Author: DESTINYAS
+ */
+
+#include<stdio.h>
+
+/**
+ * main - prints and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * followed by a new line, to the standard error.
+ * Return: is 1.
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, \
+	2015-10-19 \n");
+	return (1);
+}
