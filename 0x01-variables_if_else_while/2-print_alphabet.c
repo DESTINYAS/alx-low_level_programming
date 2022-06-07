508b5f8c4567dad769e22b45e9b81c6a00e58393
DESTINYAS <destiny.ochonogor@gmail.com>
2022-06-06 21:30:07 -0700

    abc

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+/*
+ * File: 0-positive_or_negative.c
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints Prints a random number and states whether
+ * it is positive, negative, or zero.
+ * Return: is 0.
+ */
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+	putchar(letter);
+putchar('\n');
+return (0);
+}
