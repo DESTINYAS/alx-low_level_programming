f31d5524af0ceff893263b1098347299a0aa366a
DESTINYAS <destiny.ochonogor@gmail.com>
2022-06-06 21:57:03 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+/*
+ * File: 0-positive_or_negative.c
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints Prints a random number and states whether
+ * it is positive, negative, or zero.
+ * Return: is 0.
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+	printf("%d", num);
+printf("\n");
+return (0);
+}
