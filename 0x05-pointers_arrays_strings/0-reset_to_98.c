c831addbc8eaa1d0bee51c44768b02f5bf8b306c
DESTINYAS <destiny.ochonogor@gmail.com>
2022-06-13 12:39:05 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+/*
+ * File: 0-reset_to_98.c
+ * Autor: DESTINYAS
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 -  takes a pointer to an int as parameter and /
+ * updates the  point to 98.
+ * @n: The pointer to an int.
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+
+{
+*n = 98
+}
